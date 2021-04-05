64e6d5ea51c00f8de559f7e4f50c81fd2f941fff
rhwang3 <rebeccahwang3@gmail.com>
2021-04-05 13:44:37 -0700

    Add files via upload

--- eulerBend.py
+++ eulerBend.py
@@ -19,25 +19,55 @@ def eulerBend(radius, theta, pts):
     y, x = fresnel(tprime)
     return (1/a)*x, (1/a)*y
 
+def rotate(x, y, phi):
+    a = []
+    b = []
+    c, s = np.cos(phi), np.sin(phi)
+    j = np.array([[c, s], [-s, c]])
+    m = [np.dot(j, [x[i], y[i]]) for i in range(len(x))]
+
+    for i in range(len(x)):
+        a.append(m[i][0])
+        b.append(m[i][1])
+    
+    return a, b
+
 
 # %%
+theta = np.pi/2
+pts = 100
+radius = 10
+wg_width = 1
 
+x, y = eulerBend(radius, theta/2, pts) #half the angle
+a, b = rotate(-x, y, phi = -theta)
+
+xshift = np.abs(a[-1]-x[-1])
+yshift = np.abs(b[-1]-y[-1])
+
+xfin = a + xshift
+yfin = b + yshift
+
+
+# %%
 lib = gdspy.GdsLibrary()
 date = datetime.datetime.now()
 cell = lib.new_cell('eulerBend_{}'.format(date))
 
-
-# %%
-x, y = eulerBend(10, np.pi/2, 100)
 c = lambda x, y: [c for c in zip(x,y)]
-wg_width = 1 #um
-
-
-# %%
-
 sp1 = gdspy.FlexPath(c(x,y), wg_width, gdsii_path=True)
+sp2 = gdspy.FlexPath(c(xfin, yfin), wg_width, gdsii_path=True)
 cell.add(sp1)
+cell.add(sp2)
 
 lib.write_gds('eulerBend_{}.gds'.format(date))
+
+
+# %%
+
+
+
+# %%
+
 
 
